0f11e254d1e8bb920787f611e877a8f6a0ebc3d7
morimonchan <morimorimonchan.com@gmail.com>
2022-09-23 10:46:22 +0900

    テーブルカラムの変更

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # validates :nickname,presence: true,
+  # validates :name_last,presence: true,
+  # validates :name_first,presence: true,
+  # validates :name_kana_last,presence: true,
+  # validates :name_kana_first,presence: true,
+  # validates :birthday,presence: true,
+
+  # has_many :items
+  # has_many :purchase_histories
+
+
+
+end
